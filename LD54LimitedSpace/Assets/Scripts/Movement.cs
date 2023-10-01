63c1bf944e4eeb7086e2b6e53145806e76ed899b
Bartłomiej Kawa <kawaxbart@gmail.com>
2023-10-01 16:52:02 +0200

    Make movement smooth

--- LD54LimitedSpace/Assets/Scripts/Movement.cs
+++ LD54LimitedSpace/Assets/Scripts/Movement.cs
@@ -3,10 +3,21 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    private Vector2 velocity = Vector2.zero;
+    [SerializeField]
+    private float speed = 4f;
+
     public void OnMove(InputValue value)
     {
         var v = value.Get<Vector2>();
-        Debug.Log($"move: {v}");
-        gameObject.transform.position += (Vector3)v;
+        Debug.Log($"OnMove: {v}");
+
+        v *= speed;
+        velocity = v;
+    }
+
+    private void Update()
+    {
+        gameObject.transform.position += (Vector3)velocity * Time.deltaTime;
     }
 }
