2405f665bd440c04246d0873953613263fbc6d33
Bartłomiej Kawa <kawaxbart@gmail.com>
2023-10-01 23:36:54 +0200

    Add mouse controls

--- LD54LimitedSpace/Assets/Scripts/Movement.cs
+++ LD54LimitedSpace/Assets/Scripts/Movement.cs
@@ -18,6 +18,16 @@ public class Movement : MonoBehaviour
 
     private void Update()
     {
+        var mouse = Mouse.current;
+        bool mouseIsPressed = mouse.press.value == 1;
+
+        if (mouseIsPressed)
+        {
+            var mousePosition = mouse.position.value;
+            velocity = Camera.main.ScreenToWorldPoint(mousePosition) - transform.position;
+            Debug.Log(velocity);
+        }
+        
         gameObject.transform.position += (Vector3)velocity * Time.deltaTime;
     }
 }
